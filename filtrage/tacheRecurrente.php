f1fe67025ab5e44a71a5a4a869eb147a82760ddf
Singosirutonamikaze <yaodieudonnesingo60@gmail.com>
2024-12-06 19:51:45 +0000

    Je suis sur la version backend de la gestion des tache je dois gérer la barre de recherche. Pour le moment je n'ai fait que gérer un style de formulaire dont de la gestion la creation des tache n'est pas encore terminer et aussi un exemplaire de barre de menu

--- filtrage/tacheRecurrente.php
+++ filtrage/tacheRecurrente.php
@@ -24,33 +24,40 @@
 
 <body>
     <!-- Corps de la page.-->
-    <form class="form" action="traitement.php" method="post">
+    <form class="form" action="traitement.php" method="post" id="form">
         <p class="title">New Task </p>
         <p class="message">Create a new task !! </p>
         <div class="flex">
             <label>
-                <input class="input" type="text" placeholder="" required="">
+                <input class="input" type="text" placeholder="" required="" name="name" id="name">
                 <span>Task Name</span>
+                <?php if (isset($messageErrors['name'])): ?>
+                    <p style="color:red;"> <?php echo($messageErrors['name']); ?> </p> <?php endif; ?>
             </label>
-
+           
             <label>
-                <input class="input" type="text" placeholder="" required="">
+                <input class="input" type="text" placeholder="" required="" name="priority" id="priority">
                 <span>priority</span>
+                <?php if (isset($messageErrors['priority'])): ?>
+                    <p style="color:red;"> <?php echo($messageErrors['priority']); ?> </p> <?php endif; ?>
             </label>
         </div>
-
+        
         <label>
-            <input class="input" type="email" placeholder="" required="">
-            <span>Date</span>
+            <input class="input" type="date" placeholder="" required="" name="date" id="date">
+            <?php if (isset($messageErrors['date'])): ?>
+                <p style="color:red;"> <?php echo($messageErrors['date']) ?> </p> <?php endif; ?>
         </label>
 
         <label>
-            <textarea class="input01" placeholder="" rows="3" required=""></textarea>
+            <textarea class="input01" placeholder="" rows="3" required="" name="description" id="description"></textarea>
             <span>Description</span>
+            <?php if (isset($messageErrors['description'])): ?>
+                <p style="color:red;"> <?php echo($messageErrors['description']); ?> </p> <?php endif; ?>
         </label>
         <div class="butContent">
-            <button class="submit"><a href="../filtrage/filtre.html">Create</a></button>
-            <button class="clear">Clear</button>
+            <button class="submit" name="submit" type="submit" id="submit"><a href="../filtrage/filtre.php">Create</a></button>
+            <button class="clear" name="clear" type="reset" id="clear">Clear</button>
         </div>
     </form>
 </body>
--- filtrage/traitement.php
+++ filtrage/traitement.php
@@ -1,9 +1,79 @@
 <?php
+//Dans cette section je vais gerer le formulaire et la creation de nouvelle tache
 
+//Recuperation des données via $_POST
+$name = htmlspecialchars($_POST['name']);
+$priority = htmlspecialchars($_POST['priority']);
+$date = htmlspecialchars($_POST['date']);
+$description = htmlspecialchars($_POST['description']);
 
+$errors = array(
+    'name' => '',
+    'priority' => '',
+    'date' => '',
+    'description' => ''
+);
 
+//Declaration de la variable contenant les message d'erreurs
+$messageErrors = array(
+    'name' => '',
+    'priority' => '',
+    'date' => '',
+    'description' => ''
+);
 
+//verification des erreurs
+function verification($tableau)
+{
+    $errors = "";
+    foreach ($tableau as $key) {
+        if ($tableau[$key] == "") {
+            $errors = $errors . "_" . $key;
+            header("Location:./index.php?erreur=" . $errors); //affichage des erreurs dans l'url
+        }
+    }
 
+    return $errors;
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    verification($_POST);//Appel de la fonction de verification des erreurs
+
+    if (isset($name) && isset($priority) && isset($date) && isset($description)) {
+
+    }
+}
+
+//Verification des erreurs
+if (!empty($_GET)) {
+
+    $error = $_GET['error'];//Recuperrer l'erreurs dans l'URL
+    //separation des erreurs 
+    $tableError = explode("_", $error);
+
+    //Affichage des erreurs 
+    foreach ($tableError as $value) {
+        # code...
+        switch ($value) {
+            case 'name':
+                $messageErrors['name'] = 'Veuillez entrer un nom d\'évènement valide.';
+                break;
+            case 'priority':
+                $messageErrors['name'] = 'Veuillez choisir une priorité de l\'évènement.';
+                break;
+            case 'date':
+                $messageErrors['name'] = 'Veuillez entrer une date d\'évènement valide.';
+                break;
+            case 'description':
+                $messageErrors['name'] = 'Veuillez entrer une description d\'évènement valide.';
+                break;
+            default:
+                echo ("<script alert = 'Veuillez remplir tous les champs de saisie.'></script>");
+                break;
+        }
+    }
+}
 
 
 
